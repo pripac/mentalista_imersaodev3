818c239293fbde16059f80cb6a59d0cc6b3de5b8
pripac <pristudy96@gmail.com>
2021-09-15 18:21:41 -0300

    Versão 1.0
    Versão com caixa de input que funciona e permite apenas um palpite.

--- mentalista.js
+++ mentalista.js
@@ -1,22 +1,11 @@
 var numeroSecreto = parseInt(Math.random()*10)
 
-var tentativas = 3
-
 console.log(numeroSecreto)
 
-function Chutar() { var chute = parseInt(document.getElementById("valor").value)}
+function Chutar() { var chute = parseInt(document.getElementById("valor").value)
 
-while (tentativas !== 0) {
-    
-    function Chutar() { var chute = parseInt(document.getElementById("valor").value)}
+if (chute == numeroSecreto) {alert("Parabéns, você acertou o número secreto!") }
 
- if (chute == numeroSecreto) {alert("Parabéns, você acertou o número secreto!") }
-else if (chute > numeroSecreto) {alert("O número secreto é menor")}
-else if (chute < numeroSecreto) {alert("O numero secreto é maior")}
-else {alert("Você errou o número secreto.")}
+else {alert("Você errou o número secreto.")} 
 
-tentativas = tentativas -1}
-
-if (chute !== numeroSecreto)  {alert("Suas tentativas acabaram! O número secreto era " + numeroSecreto)}
-
-
+alert("O número secreto era " + numeroSecreto) }

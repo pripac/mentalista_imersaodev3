df3b40ebeb96faafe2f39063b5059398108a585b
pripac <pristudy96@gmail.com>
2021-09-24 14:27:20 -0300

    Alteração txt html

--- mentalista.js
+++ mentalista.js
@@ -2,10 +2,24 @@ var numeroSecreto = parseInt(Math.random()*10)
 
 console.log(numeroSecreto)
 
-function Chutar() { var chute = parseInt(document.getElementById("valor").value)
+var i= 3
+
+function Chutar() { 
+
+
+
+while (i > 0 ){        
+var chute = parseInt(document.getElementById("valor").value)
 
 if (chute == numeroSecreto) {alert("Parabéns, você acertou o número secreto!") }
 
+else if (chute > numeroSecreto) {alert("O número secreto é menor")}
+else if (chute < numeroSecreto) {alert("O numero secreto é maior")}
+
 else {alert("Você errou o número secreto.")} 
 
-alert("O número secreto era " + numeroSecreto) }
+i--}
+
+alert("O número secreto era " + numeroSecreto)  }
+
+
